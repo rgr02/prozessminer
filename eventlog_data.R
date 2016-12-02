2fa4aa84f089de6981b4747be6d39096f9a55778
rgr02 <rudolf.gruber@khg.jku.at>
2016-12-02 15:19:03 +0100

    Hilfstabelle erstellt und persistiert

--- eventlog_data.R
+++ eventlog_data.R
@@ -1,71 +1,140 @@
 #install.packages("XLConnect")
-#install.packages("XLConnectJars")
 
-#library(XLConnectJars)
 library(XLConnect)
 
 #############################################################################
-# (1) LADEN DER INPUTDATEN
+# LADEN DER INPUTDATEN
 # file.choose() öffnet einen Editor
 srcFile <- file.choose()
 #############################################################################
 
-#############################################################################
-# (2) ERSTELLEN DER DATAFRAMES
+################################################################################
+# ERSTELLEN DER DATAFRAMES und umbenennen von Variablen
 kreditor = readWorksheetFromFile(srcFile,sheet="Kreditor", header=T)
-aenderungsHist = readWorksheetFromFile(srcFile,sheet="Änderungshistorie", header=T)
+änderungsHist = readWorksheetFromFile(srcFile,sheet="Änderungshistorie", header=T)
 bestellung = readWorksheetFromFile(srcFile,sheet="Bestellung", header=T)
 bestellPos = readWorksheetFromFile(srcFile,sheet="Bestellposition", header=T)
 warenEingang = readWorksheetFromFile(srcFile,sheet="Wareneingang", header=T)
 rechnung = readWorksheetFromFile(srcFile,sheet="Rechnung", header=T)
 zahlung = readWorksheetFromFile(srcFile,sheet="Zahlung", header=T)
+# Soweit funktioniert einmal das Laden der Daten
+# Umbenennen von Spaltennamen um Namenskonflikte in der Hilfstabelle zu umgehen
+names(kreditor)[names(kreditor) == 'ErstellTS'] <- 'ErstellTS_Kreditor'
+names(bestellung)[names(bestellung) == 'ErstellTS'] <- 'ErstellTS_Bestellung'
+names(bestellPos)[names(bestellPos) == 'PosNr'] <- 'PosNr_Bestellpos'
+names(bestellPos)[names(bestellPos) == 'ErstellTS'] <- 'ErstellTS_Bestellpos'
+names(warenEingang)[names(warenEingang) == 'EingangsTS'] <- 'EingangsTS_WAE'
+names(zahlung)[names(zahlung) == 'ZahlTS'] <- 'ZahlTS_Zahlung'
+################################################################################
 
-#######################################################################
-#(3) Bereinigung von Fehlern
+################################################################################
+# DATENMANIPULATIONEN
+# Zeilen mit NA in Joinattributen löschen
 
-no_bestellNr<-which(is.na(warenEingang$BestellNr))
-warenEingang<- warenEingang[-no_bestellNr,]
-######################################################################
-# (4) Joinen der Tabellen
+if ('True' %in% is.na(warenEingang$BestellNr)) {
+  no_BN_WAE<-which(is.na(warenEingang$BestellNr))
+  warenEingang<- warenEingang[-no_BN_WAE,]
+  # Funktioniert an der Stelle. NA in Wareneingang gelöscht.
+}
 
-#Outer join: merge(x = df1, y = df2, by = "CustomerId", all = TRUE)
-#Left outer: merge(x = df1, y = df2, by = "CustomerId", all.x = TRUE)
-#Right outer: merge(x = df1, y = df2, by = "CustomerId", all.y = TRUE)
-#Cross join: merge(x = df1, y = df2, by = NULL)
+if ('True' %in% is.na(rechnung$BestellNr)) {
+  no_BN_RE<-which(is.na(rechnung$BestellNr))
+  rechnung<-rechnung[-no_BN_RE,]
+  # Funktioniert an der Stelle. NA in Rechnung gelöscht.
+}
+?is.element
 
-head(bestellung)
-str(bestellPos)
+if ('True' %in% is.na(bestellung$BestellNr)) {
+  no_BN_BE<-which(is.na(bestellung$BestellNr))
+  bestellung<-bestellung[-no_BN_BE,]
+  # Keine NA in BestellNr. 
+  # Zweite Zeile negiert und löscht alle Einträge !!
+  # Daher sind die if Abfragen notwendig um Generizität für verschiedene Input-
+  # daten zu gewährleisten.
+}
 
-#bestellung mit bestellPos
-merge_bb<-merge(x=bestellung, y=bestellPos, by= "BestellNr", all=TRUE)
-dim(merge_bb)
-dim(bestellung)
-dim(bestellPos)
-bestellPos$BestellNr%in%merge_bb$BestellNr
-bestellung$BestellNr%in%merge_bb$BestellNr
+if ('True' %in% is.na(bestellPos$BestellNr)) {
+  no_BN_BPos<-which(is.na(bestellPos$BestellNr))
+  bestellPos<-bestellPos[-no_BN_BPos,]
+}
 
-#join mit wareneingang
-str(warenEingang)
-merge_bbw<-merge(x=merge_bb, y=warenEingang, by="BestellNr", all=TRUE)
-dim(merge_bbw)
-dim(warenEingang)
-dim(merge_bb)
-merge_bb$BestellNr%in%merge_bbw$BestellNr
-merge_bbw$BestellNr%in%merge_bb$BestellNr
+if ('True' %in% is.na(kreditor$KredNr)) {
+  no_KR_Kredit<-which(is.na(kreditor$KredNr))
+  kreditor<-kreditor[-no_KR_Kredit,]
+}
 
-#join mit kreditor
-str(kreditor)
-str(merge_bbw)
-merge_bbwk<-merge(x=merge_bbw, y=kreditor, by.x="KredNr.x", by.y="KredNr", all=TRUE)
+if ('True' %in% is.na(zahlung$KredNr)) {
+  no_KR_zahlung<-which(is.na(zahlung$KredNr))
+  zahlung<-zahlung[-no_KR_zahlung,]
+}
+################################################################################
 
-merge_bbw$BestellNr%in%merge_bbwk$BestellNr
-merge_bbwk$BestellNr%in%merge_bbw$BestellNr# False Fälle genauer Ansehen
+################################################################################
+# verknüpfen der Tabellen
+# Äquivalenzen zu SQL Joins in R
+# Inner Join wie Outer Join ohne , all = True
+# Outer join: merge(x = df1, y = df2, by = "CustomerId", all = TRUE)
+# Left outer: merge(x = df1, y = df2, by = "CustomerId", all.x = TRUE)
+# Right outer: merge(x = df1, y = df2, by = "CustomerId", all.y = TRUE)
+# Cross join: merge(x = df1, y = df2, by = NULL)
 
-merge_bbw$KredNr.x%in%merge_bbwk$KredNr.x
-merge_bbwk$KredNr.x%in%merge_bbw$KredNr.x# False Fälle genauer Ansehen
+# Left Join Bestellung mit BestellPos
+merge_bb<-merge(x=bestellung, y=bestellPos, by= "BestellNr", all.x = TRUE)
 
-#FAlse Fälle werden ansgeschaut -> keine Ahnung was wir damit machen
-merge_bbw[which(!(merge_bbwk$BestellNr%in%merge_bbw$BestellNr)),]
-merge_bbwk[which(!(merge_bbwk$KredNr.x%in%merge_bbw$KredNr.x)),]
+# Left Join mit Wareneingang
+merge_bbw<-merge(x=merge_bb, y=warenEingang, by="BestellNr", all.x = TRUE)
 
-str(rechnung)
+# Left Join mit Rechnung
+merge_bbwr<-merge(x=merge_bbw, y=rechnung, by="BestellNr", all.x = TRUE)
+
+# Left Join mit Zahlung
+merge_bbwrz<-merge(x=merge_bbwr, y=zahlung, by.x="KredNr.x", by.y="KredNr", all.x = TRUE)
+
+# Left Join mit Kreditor
+merge_bbwrzk<-merge(x=merge_bbwrz, y=kreditor, by.x="KredNr.x", by.y="KredNr", all.x = TRUE)
+
+# Änderungshistorie
+# Tabelle Kreditor
+kredit<-which(änderungsHist$Tabelle == "Kreditor")
+änderungsHist_Kredit <- änderungsHist[kredit,]
+merge_bbwrzk_ak<-merge(x=merge_bbwrzk, y=änderungsHist_Kredit, by.x="KredNr.x", by.y="ID", all.x = TRUE)
+
+# Tabelle Bestellung
+bestell<-which(änderungsHist$Tabelle == "Bestellung")
+änderungsHist_bestell <- änderungsHist[bestell,]
+merge_bbwrzk_akb<-merge(x=merge_bbwrzk_ak, y=änderungsHist_bestell, by.x="BestellNr", by.y="ID", all.x = TRUE)
+
+# Tabelle Bestellposition
+# Hilfsspalte mit geänderter ID - erste Nummer weggeschnitten
+x<-substr(änderungsHist$ID,2,7)
+änderungsHist$IDTrim=x
+bestellP<-which(änderungsHist$Tabelle == "Bestellposition")
+änderungsHist_bestellP <- änderungsHist[bestellP,]
+merge_bbwrzk_akbb<-merge(x=merge_bbwrzk_akb, y=änderungsHist_bestellP, by.x="BestellNr", by.y="IDTrim", all.x = TRUE)
+################################################################################
+
+################################################################################
+# Festlegen der benötigten Spalten für die Hilfstabelle
+spalten<-c("PosNr_Bestellpos","BestellNr","ErstellTS_Bestellpos","ErstellTS_Bestellung","ErstellTS_Kreditor","EingansDat",
+           "RechnungsDatum","EingangsTS_WAE","ZahlTS_Zahlung","AenderTS","Feld","Wert_neu","Tabelle")
+
+# Relevante Ergebnisse als Hilfstabelle. Input für den a-Algorithmus
+hilfstabelle <- merge_bbwrzk_akbb[,spalten]
+################################################################################
+
+################################################################################
+# PERSISTIEREN DER HILFSTABELLE
+# workbook anscheinend für createSheet benötigt.
+
+# Implementierung laut Doku
+wb <- loadWorkbook(srcFile, create = T)
+createSheet(wb,"Hilfstabelle") # Name für das Arbeitsblatt
+setColumnWidth(wb,"Hilfstabelle",column = c(1:13),5000)
+saveWorkbook(wb)
+writeWorksheetToFile(srcFile, hilfstabelle, sheet = "Hilfstabelle") 
+################################################################################
+
+################################################################################
+# NEXT STEPS HERE
+
+bestellNr <- unique(hilfstabelle$BestellNr)
